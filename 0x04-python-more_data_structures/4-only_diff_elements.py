072dae07df61c998b5b0210b551c741c31235b88
gilly002 <gilbertamenorhu@gmail.com>
2023-11-09 03:43:32 -0800

    4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
-
-set_1 = { "Python", "C", "Javascript" }
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
